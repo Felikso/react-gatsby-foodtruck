f93339032e7b6bac519daabcff1d8333effa2fe2
Felikso <aleksandrasniegucka@wp.pl>
2021-04-28 00:01:01 +0200

    added lazy to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
+import { useHasBeenVisible } from '../hooks/useVisibility';
 import Layout from "../components/Layout/index.js"
 import Seo from "../components/seo"
 import Hero from '../components/Hero'
@@ -7,23 +8,28 @@ import Hero from '../components/Hero'
 import Testimonials from "../components/Testimonials"
 import Stats from "../components/Stats"
 import Email from "../components/Email"
+import FullWidthSection from '../components/FullWidthSection';
 import FeaturedProductsBox from '../containers/FeaturedProductsBox';
 
 function IndexPage() {
-
-
+  const halfPage = useRef();
+  const preload = useRef();
+  const hasScrolled = useHasBeenVisible(halfPage);
+  const isScrolling = useHasBeenVisible(preload);
   return(
     <Layout>
       <Seo title="Home" />
       <Hero />
-
+      {hasScrolled || isScrolling ? (
         <>
       <FeaturedProductsBox heading="Serdecznie polecamy" />
       <Testimonials />
       <Stats />
       <Email />
         </>
-
+      ) : (
+        <FullWidthSection ref={halfPage} height='2286px' minHeight='3448px' />
+      )}
 
     </Layout>
   )
